ac50ce758efb93bc9e18c9b1ed4d677c1b414bbc
Clay Kuznia <clay-kuznia@hotmail.com>
2016-06-28 16:02:16 -0600

    Changed Algorithm
    Algorithm for day/night cycle was changed. As well as some other tweaks.
    Now uses two temperature sensors.

--- garage_fan/garage_fan.ino
+++ garage_fan/garage_fan.ino
@@ -1,103 +1,43 @@
 #include <dht.h>
 
 boolean fanPower = false;
-const int fanPin = 3;
+const int fanPin = 2;
 
-// Temperature sensor (DHT22, aka: AM2302)
-#define DHT22_PIN 2
-double tempF, coldTemp = 1000, hotTemp = -1000;
-const double START_TEMP = 72.0; // temperature (Fahrenheit) must be at or below this level for the fan to start running.
+double externalTemp, internalTemp, coldTemp = 1000;
+//const double SETPOINT = 70.0; // temperature (Fahrenheit) must be at or below this level for the fan to start running.
 boolean cooling;
 
-dht DHT;
+// Temperature sensors (DHT22, aka: AM2302)
+dht EXTERNAL_DHT, INTERNAL_DHT;
+#define INTERNAL_PIN 3
+#define EXTERNAL_PIN 4
 
 void setup() {
   Serial.begin(9600);
   pinMode(fanPin, OUTPUT);
+  delay(1000);
+  Serial.println("\n\n");
   
-  // Use a base temperature reading and compare it to a temperature reading
-  // some time in the future to determine if its heating up or cooling down. This
-  // will tell us what cycle/"time" of the day it is.
-  updateTemp();
-  double baseTemp = tempF;
-  double tolerance = 1.25;
-
-  Serial.println("Base Temp: " + String(baseTemp));
-
-  // Waiting for the temperature to change a certain amount to determine
-  // if it is heating up or cooling down.
-  while(abs(baseTemp - tempF) < tolerance) {
-    Serial.println("Temp checked. " + String(tempF) + "F");
-
-    // Wait just over two seconds (the sensor can only be read about once every two seconds)
-    delay(2100);
-
-    // Gather another temp sample
-    updateTemp();
-  }
-
-  // There is now a significant difference in temperature
-  // Now checking if it is heating up or cooling down outside
-  if(baseTemp - tempF > 0) cooling = true;
-  else cooling = false;
-  delay(2100);
+  // Ensure fan is off
+  digitalWrite(fanPin, LOW);
 }
 
 void loop() {
-  updateTemp();
+  externalTemp = getTemp(EXTERNAL_DHT, EXTERNAL_PIN);
+  internalTemp = 70.0; // Will be changed when sensor is added to circuit
   updateFan();
-  delay(2100);
-
-  //printing out values
-  Serial.print("Temp: " + String(tempF));
-  Serial.print("\tLow: " + String(coldTemp));
-  Serial.print("  High: " + String(hotTemp));
-  String s = cooling ? "true" : "false";
-  Serial.print("\tCooling: " + s);
-  String s2 = digitalRead(fanPin) == HIGH ? "ON" : "OFF";
-  Serial.println("  Fan: " + s2);
+  displayValues();
+  
+  waitMin(5);
 }
 
-void updateFan() {
-  if(cooling) { // Outside is cooling down
-    if(digitalRead(fanPin) == HIGH) { // Fan is on
-      // Update coldest temperature
-      if(tempF < coldTemp) coldTemp = tempF;
-      
-      // Checking if temperature is heating up
-      double tolerance = 1.0;
-      if(tempF >= coldTemp + tolerance) { // Outside is now heating back up
-        digitalWrite(fanPin, LOW);
-        coldTemp = 1000;
-        cooling = false;
-      }
-    }
-    else { // Fan is off
-      // Checking if temperature is low enough
-      if(tempF <= START_TEMP) digitalWrite(fanPin, HIGH);
-    }
-    
-  }
-  else { // Outside is heating up
-     // Update hottest temperature
-      if(tempF > hotTemp) hotTemp = tempF;
-      
-      // Checking if temperature is cooling down
-      double tolerance = 1.0;
-      if(tempF <= hotTemp - tolerance) { // Outside is now cooling back down
-        hotTemp = -1000;
-        cooling = true;
-      }
-  }
-}
-
-void updateTemp() {
+double getTemp(dht DHT, int pin) {
   // READ DATA
-  int chk = DHT.read22(DHT22_PIN);
+  int chk = DHT.read22(pin);
   switch (chk)
   {
     case DHTLIB_OK:
-      tempF = (DHT.temperature * (9.0/5.0)) + 32;
+      return (DHT.temperature * (9.0/5.0)) + 32.0;
       break;
     case DHTLIB_ERROR_CHECKSUM: 
       Serial.println("Checksum error"); 
@@ -107,7 +47,61 @@ void updateTemp() {
       break;
     default: 
       Serial.println("Unknown error");
-      break;
   }
-  if(chk != DHTLIB_OK) tempF = 999;
+  return 999;
+}
+
+void updateFan() {
+  // Take note of fan status
+  boolean fanPower = digitalRead(fanPin) == HIGH;
+  
+  // The fan only runs if the difference between internal and external temperatures are greater than +2.0 degrees
+  double diff = internalTemp - externalTemp;
+  if(diff >= 2.0) {
+    // Recording the lowest temperature
+    if(externalTemp < coldTemp) coldTemp = externalTemp;
+    
+    // Now checking to see if the temperature is heating up, the fan will run only when
+    // the temperature is currently dropping. Once the temperature begins to rise the fan
+    // will be shut off.
+    double tolerance = 1.75;
+    cooling = (externalTemp <= coldTemp + tolerance);
+
+    // Air is cooling down
+    if(cooling) {
+      // If fan is off, turn it on
+      if(!fanPower) digitalWrite(fanPin, HIGH);
+    }
+    // Air is heating up
+    else {
+      // If fan is on, turn it off
+      if(fanPower) digitalWrite(fanPin, LOW);
+    }
+  }
+  else {
+    // If fan is on, turn it off
+    if(fanPower) digitalWrite(fanPin, LOW);
+    coldTemp = 1000;
+  }
+}
+
+void displayValues() {
+  // Displaying values
+  Serial.print("External Temp: " + String(externalTemp));
+  Serial.print("\tInternal Temp: " + String(internalTemp));
+  Serial.print("\tLow Temp: " + String(coldTemp));
+  String s = cooling ? "true" : "false";
+  Serial.print("\t|\tCooling: " + s);
+  String s2 = digitalRead(fanPin) == HIGH ? "ON" : "OFF";
+  Serial.println("\tFan: " + s2);
+}
+
+void waitMin(int minutes) {
+  for(int x = 0; x < minutes; x++) {
+    // Wait 1 minute
+    for(int n = 0; n < 60; n++) {
+      // Wait 1 second
+      delay(1000);
+    }
+  }
 }
